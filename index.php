d9be5aa49cf7ae90ae37efd6936f63f7ae9254bd
Isma-ae <36768882+Isma-ae@users.noreply.github.com>
2025-10-24 22:37:47 +0700

    ทดสอบ

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 <html lang="en">
 
 <head>
-    <title>Home</title>
+    <title>Homeaaa</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" href="images/icons/logo-mini.png" />
